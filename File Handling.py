eea6f794df1b100449b2a01f5bf94fee87babdb3
Patel Darshil <70016353+Darshil987@users.noreply.github.com>
2020-10-15 10:43:44 +0530

    Update File Handling.py
    read the content of file

--- File Handling.py
+++ File Handling.py
@@ -2,7 +2,12 @@ try:
     a = input("Enter File Name:")
     f = open(a, 'rt')
     if f.read:
-        print("File Found")
+        b = input("File Found.Do you want to read (Y or N) :")
+        if b == 'y' or b == 'Y':
+            print(f.read())
+        else:
+            print("Ok Bye, Have a nice day")
+    f.close()
 
 except FileNotFoundError:
     print("Not Found")
