0e7e7b8e3a4d7de5e928b468450c6391bf589a33
Patel Darshil <70016353+Darshil987@users.noreply.github.com>
2020-10-15 22:25:54 +0530

    Add table

--- PyMYSQL.py
+++ PyMYSQL.py
@@ -1,6 +1,8 @@
 import mysql.connector as mcom
 
-conn = mcom.connect(host="localhost", user="root", password="")
-cursor = conn.cursor()
-
-cursor.execute("CREATE DATABASE abc")
+conn = mcom.connect(host="localhost", user="root", password="", database="abc")
+if conn:
+    cursor = conn.cursor()
+    cursor.execute("CREATE TABLE login(srno INT AUTO_INCREMENT PRIMARY KEY, username VARCHAR(8), password VARCHAR(20))")
+else:
+    print("Error")
