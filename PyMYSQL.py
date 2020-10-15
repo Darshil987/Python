98b102c47fdf3cda2831d75ab6d663be29ae6d47
Patel Darshil <70016353+Darshil987@users.noreply.github.com>
2020-10-15 22:31:50 +0530

    Insert into table single row

--- PyMYSQL.py
+++ PyMYSQL.py
@@ -3,6 +3,9 @@ import mysql.connector as mcom
 conn = mcom.connect(host="localhost", user="root", password="", database="abc")
 if conn:
     cursor = conn.cursor()
-    cursor.execute("CREATE TABLE login(srno INT AUTO_INCREMENT PRIMARY KEY, username VARCHAR(8), password VARCHAR(20))")
+    sql = "INSERT INTO login(username, password) VALUES('Darshil','123456')"
+    cursor.execute(sql)
+    conn.commit()
+    print(cursor.rowcount,"Rows Inserted")
 else:
     print("Error")
