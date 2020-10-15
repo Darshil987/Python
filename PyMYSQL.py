b4fba9a6c4278861ac6dc2df9146f06425504687
Patel Darshil <70016353+Darshil987@users.noreply.github.com>
2020-10-15 22:37:15 +0530

    Fetch DATA

--- PyMYSQL.py
+++ PyMYSQL.py
@@ -3,14 +3,10 @@ import mysql.connector as mcom
 conn = mcom.connect(host="localhost", user="root", password="", database="abc")
 if conn:
     cursor = conn.cursor()
-    sql = "INSERT INTO login(username, password) VALUES(%s,%s)"
-    val =[
-        ('Darshil1','132'),
-        ('Darshil12','123'),
-        ('Darshil13','312'),
-    ]
-    cursor.executemany(sql, val)
-    conn.commit()
-    print(cursor.rowcount,"Rows Inserted")
+    sql = "SELECT * FROM login WHERE username='Darshil'"
+    cursor.execute(sql)
+    x = cursor.fetchall()
+    for i in x:
+        print(i)
 else:
     print("Error")
