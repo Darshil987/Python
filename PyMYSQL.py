c8c97cb28d9957f36bf62998ad14b7153fc2dbd1
Patel Darshil <70016353+Darshil987@users.noreply.github.com>
2020-10-15 22:39:23 +0530

    Fetch DATA(prevent sql injecction)

--- PyMYSQL.py
+++ PyMYSQL.py
@@ -3,8 +3,9 @@ import mysql.connector as mcom
 conn = mcom.connect(host="localhost", user="root", password="", database="abc")
 if conn:
     cursor = conn.cursor()
-    sql = "SELECT * FROM login WHERE username='Darshil'"
-    cursor.execute(sql)
+    sql = "SELECT * FROM login WHERE username=%s"
+    val = 'Darshil',
+    cursor.execute(sql, val)
     x = cursor.fetchall()
     for i in x:
         print(i)
