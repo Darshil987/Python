6f5202959416ebb8adc0d65bf6d2f0f44f439cfd
Patel Darshil <70016353+Darshil987@users.noreply.github.com>
2020-10-15 11:00:32 +0530

    check file , read, write
    Firstly it check file exists, if not then it will ask to create then ask to add content.
    and if found read content

--- File Handling.py
+++ File Handling.py
@@ -2,7 +2,7 @@ try:
     a = input("Enter File Name:")
     f = open(a, 'rt')
     if f.read:
-        b = input("File Found.Do you want to read (Y or N) :")
+        b = input("File Found.Do you want to read (Y or N):")
         if b == 'y' or b == 'Y':
             print(f.read())
         else:
@@ -11,3 +11,22 @@ try:
 
 except FileNotFoundError:
     print("Not Found")
+    a = input("Do you want to create File (Y or N):")
+    if a == 'y' or a == 'Y':
+        b = input("Enter File Name:")
+        try:
+            f = open(b, 'x')
+            print("File Created")
+            c = input("Do you want to add content (Y or N):")
+            if c == 'Y' or c == 'y':
+                z = input("Enter Content:")
+                f = open(b, 'w')
+                if f.write(z):
+                    print("Write Successfully")
+            f.close()
+        except FileExistsError:
+            print("File already exists")
+    elif a == 'N' or a == 'n':
+        print("Thank You, Have a nice day")
+    else:
+        print("Please enter Uppercase/lowercase (Y or N)")
