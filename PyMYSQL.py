d155e48ffe348e788066c29a7ab172a088f16941
Patel Darshil <70016353+Darshil987@users.noreply.github.com>
2020-10-15 22:21:35 +0530

    Create Database

--- PyMYSQL.py
+++ PyMYSQL.py
@@ -1,8 +1,6 @@
 import mysql.connector as mcom
 
 conn = mcom.connect(host="localhost", user="root", password="")
-cursor =conn.cursor()
+cursor = conn.cursor()
 
-cursor.execute("SHOW DATABASES")
-for i in cursor:
-    print(i)
+cursor.execute("CREATE DATABASE abc")
