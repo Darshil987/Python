0f6f6831f256b39a152cbe8d66aaf200668dfaef
Patel Darshil <70016353+Darshil987@users.noreply.github.com>
2020-10-15 22:33:17 +0530

    Prevent sql Injection

--- PyMYSQL.py
+++ PyMYSQL.py
@@ -3,8 +3,9 @@ import mysql.connector as mcom
 conn = mcom.connect(host="localhost", user="root", password="", database="abc")
 if conn:
     cursor = conn.cursor()
-    sql = "INSERT INTO login(username, password) VALUES('Darshil','123456')"
-    cursor.execute(sql)
+    sql = "INSERT INTO login(username, password) VALUES(%s,%s)"
+    val = 'Darshil', '123456'
+    cursor.execute(sql, val)
     conn.commit()
     print(cursor.rowcount,"Rows Inserted")
 else:
