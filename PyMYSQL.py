eac5bf8536265faa411e937ccc7f7fd7ef591686
Patel Darshil <70016353+Darshil987@users.noreply.github.com>
2020-10-17 13:03:56 +0530

    Join Table

--- PyMYSQL.py
+++ PyMYSQL.py
@@ -3,7 +3,11 @@ import mysql.connector as mcom
 conn = mcom.connect(host="localhost", user="root", password="", database="abc")
 if conn:
     cursor = conn.cursor()
-    sql = "SELECT * FROM login LIMIT 2"
+    sql = "SELECT\
+           login.username as user,\
+           cart.product as product\
+           FROM login\
+           INNER JOIN cart ON login.username=cart.username"
     cursor.execute(sql)
     x = cursor.fetchall()
     for i in x:
