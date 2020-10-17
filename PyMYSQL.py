6bbe40f359815f3f02c2645a6bd809ac2f66877c
Patel Darshil <70016353+Darshil987@users.noreply.github.com>
2020-10-17 13:05:55 +0530

    DROP table

--- PyMYSQL.py
+++ PyMYSQL.py
@@ -3,14 +3,8 @@ import mysql.connector as mcom
 conn = mcom.connect(host="localhost", user="root", password="", database="abc")
 if conn:
     cursor = conn.cursor()
-    sql = "SELECT\
-           login.username as user,\
-           cart.product as product\
-           FROM login\
-           INNER JOIN cart ON login.username=cart.username"
+    sql = "DROP TABLE login"
     cursor.execute(sql)
-    x = cursor.fetchall()
-    for i in x:
-        print(i)
+
 else:
-    print("Error")
+    print("ERROR")
