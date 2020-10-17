c3c6d83bbddb9e70ddf1a94f34834b3dbcba1c3f
Patel Darshil <70016353+Darshil987@users.noreply.github.com>
2020-10-17 13:08:14 +0530

    DROP DATABASE

--- PyMYSQL.py
+++ PyMYSQL.py
@@ -3,7 +3,7 @@ import mysql.connector as mcom
 conn = mcom.connect(host="localhost", user="root", password="", database="abc")
 if conn:
     cursor = conn.cursor()
-    sql = "DROP TABLE login"
+    sql = "DROP DATABASE abc"
     cursor.execute(sql)
 
 else:
