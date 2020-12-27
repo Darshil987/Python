f0ee517067d64e07f8048d1e96f9d8c10c9ceca6
Patel Darshil <70016353+Darshil987@users.noreply.github.com>
2020-12-27 17:37:54 +0530

    Update PyMongoDB.py
    Find document

--- PyMongoDB.py
+++ PyMongoDB.py
@@ -6,13 +6,6 @@ mydb = myclient['test']
 
 mycol = mydb['customers']
 
-list =[
-    {"_id": 1, "name": "Darshil", "EnNO": 170820107021 },
-    {"_id": 2, "name": "ABC", "EnNO": 1234 },
-    {"_id": 3, "name": "DEF", "EnNO": 5678 }
-]
-x = mycol.insert_many(list)
+x = mycol.find_one()
 
-if x:
-    print("Enter Successfully")
-    print(x.inserted_ids)
+print(x)
